369e055740882cd5d362b220002e44f914a50e35
Ayush Kumar <ayush21929a@gmail.com>
2026-05-10 16:37:57 +0530

    s

--- server-talksy/src/routes/messageRoutes.js
+++ server-talksy/src/routes/messageRoutes.js
@@ -1,34 +1,34 @@
 import {
- sendMessage,
- getMessages,
- getConversations,
- markAsRead,
- deleteMessage,
- markAsDelivered,
- uploadMedia
+    sendMessage,
+    getMessages,
+    getConversations,
+    markAsRead,
+    deleteMessage,
+    markAsDelivered,
+    uploadMedia
 } from "../controllers/messageController.js";
 
 export default async function messageRoutes(fastify, options) {
 
- // Send a new message
- fastify.post("/send-message", sendMessage);
+    // Send a new message
+    fastify.post("/send-message", sendMessage);
 
- // Upload media (image/video/audio) to Cloudinary
- fastify.post("/upload-media", uploadMedia);
+    // Upload media (image/video/audio) to Cloudinary
+    fastify.post("/upload-media", uploadMedia);
 
- // Get paginated messages between two users
- fastify.get("/messages/:senderId/:receiverId", getMessages);
+    // Get conversations list (chat inbox) for a user
+    fastify.get("/conversations/:userId", getConversations);
 
- // Get conversations list (chat inbox) for a user
- fastify.get("/conversations/:userId", getConversations);
+    // Get paginated messages between two users
+    fastify.get("/:senderId/:receiverId", getMessages);
 
- // Mark messages as read (bulk)
- fastify.put("/read", markAsRead);
+    // Mark messages as read (bulk)
+    fastify.put("/read", markAsRead);
 
- // Delete a message
- fastify.put("/delete/:messageId", deleteMessage);
+    // Delete a message
+    fastify.put("/delete/:messageId", deleteMessage);
 
- // Mark pending messages as delivered (called when app comes to foreground)
- fastify.post("/mark-delivered", markAsDelivered);
+    // Mark pending messages as delivered (called when app comes to foreground)
+    fastify.post("/mark-delivered", markAsDelivered);
 
 }
